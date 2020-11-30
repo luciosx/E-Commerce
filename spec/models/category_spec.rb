bd0797c015d9063a7c8b04b9fe724d160ab9e351
luciosx <luciosx@hotmail.com>
2020-11-30 17:24:51 -0300

    Creates productCategory model and other associations

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+
+  # O "dependent destroy" serve para excluir o produto que esteja associado a categoria que foi excluida
+  it { is_expected.to have_many(:product_categories).dependent(:destroy) }
+  it { is_expected.to have_many(:products).through(:product_categories) }
 end
