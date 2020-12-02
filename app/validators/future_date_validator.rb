2f42258f3a472777f5a0a692c0ab8b5c83f2c3b8
luciosx <luciosx@hotmail.com>
2020-12-02 11:10:12 -0300

    Finalizes the customized validation of coupon model

--- app/validators/future_date_validator.rb
+++ app/validators/future_date_validator.rb
@@ -1,3 +1,8 @@
 class FutureDateValidator < ActiveModel::EachValidator
-    
+    def validate_each(record, attribute, value)
+        if value.present? && value <= Time.zone.now
+            message = options[:message] || :future_date # Essa mensagem de erro vem do I18N, na pasta locales no arquivo "custom_validations.yml"
+            record.errors.add(attribute, message)
+        end
+    end
 end
