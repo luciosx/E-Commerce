481aede6373bbb58af12087c89b5d28b99862707
luciosx <luciosx@hotmail.com>
2020-12-02 12:04:54 -0300

    Product image addition and configuration

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     sequence(:name) { |n| "MyString #{n}" }
     description { Faker::Lorem.paragraph } #gera um paragrafo aleatorio
     price { Faker::Commerce.price(range: 100.0..400.0) } #gera um preço aleatorio dentro do range de 100 a 400
+    image { Rack::Test::UploadedFile.new(Rails.root.join("spec/support/images/product_image.png")) }
     
     after :build do |product|
       product.productable = create(:game)
