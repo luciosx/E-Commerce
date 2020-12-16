5d08c5a46c0a57ea9442d601d50ac707213ba6a9
luciosx <luciosx@hotmail.com>
2020-12-16 11:10:57 -0300

    Remove an category

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -1,5 +1,8 @@
 module Admin::V1
     class CategoriesController < ApiController
+        # Executa o load_category somente antes do update e do destroy
+        before_action :load_category, only: [:update, :destroy]
+
         def index
             @categories = Category.all
         end
@@ -11,12 +14,21 @@ module Admin::V1
         end
 
         def update
-            @category = Category.find(params[:id])
             @category.attributes = category_params
             save_category!
         end
 
+        def destroy
+            @category.destroy!
+        rescue
+            render_error(fields: @category.errors.messages)
+        end
+
         private
+
+        def load_category
+            @category = Category.find(params[:id])
+        end
 
         def category_params
             return {} unless params.has_key?(:category)
