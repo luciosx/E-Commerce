5b46aff18090b5bca19f73ad47586566d11fd419
luciosx <luciosx@hotmail.com>
2020-12-22 20:40:33 -0300

    Add search for name on the models

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -7,4 +7,6 @@ RSpec.describe Category, type: :model do
   # O "dependent destroy" serve para excluir o produto que esteja associado a categoria que foi excluida
   it { is_expected.to have_many(:product_categories).dependent(:destroy) }
   it { is_expected.to have_many(:products).through(:product_categories) }
+
+  it_behaves_like "name searchable concern", :category 
 end
